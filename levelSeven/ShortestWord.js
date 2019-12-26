4a64de8afaf3b49cf88dfcb13491046c4f6c452b
saruit0105 <saruits2@gmail.com>
2019-12-26 00:44:56 -0500

    refactored

--- levelSeven/ShortestWord.js
+++ levelSeven/ShortestWord.js
@@ -1,7 +1,5 @@
 function findShort(s) {
   let splitStr = s.split(" ");
-  sortedArr = splitStr.sort((a, b) => {
-    return a.length - b.length;
-  });
+  sortedArr = splitStr.sort((a, b) => a.length - b.length);
   return sortedArr[0].length;
 }
